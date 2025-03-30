6b9adbd71b307ce7068da63420470bf1338463ec
Kevin Kegan Olome <kevin.olome@griffinglobaltech.com>
2025-03-30 12:30:36 +0300

    Adding knowledgebase

--- index.py
+++ index.py
@@ -2,7 +2,12 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
 # Create chatbot instance with correct logic adapter name
-bot = ChatBot("chatbot", read_only=False, logic_adapters=["chatterbot.logic.BestMatch"])
+bot = ChatBot("chatbot", read_only=False, logic_adapters=[
+    {"import_path":"chatterbot.logic.BestMatch",
+     "default_response":"Sorry I don't have an answer",
+     "maximum_similarity_threshold":0.9
+     }
+     ])
 
 # Training data
 list_to_train = [
@@ -11,7 +16,12 @@ list_to_train = [
     "What's your name?",
     "I'm a chatbot",
     "How old are you?",
-    "I'm ageless!"
+    "I'm ageless!",
+    "Why are you so mad",
+    "I'm not!"
+    "Do you have an iphone?"
+    "I've everything",
+    "What's your Favorite food?",
 ]
 
 # Train the chatbot
@@ -19,3 +29,7 @@ trainer = ListTrainer(bot)
 trainer.train(list_to_train)
 
 print("Chatbot trained successfully!")
+
+while True:
+    user_response= input("User: ")
+    print("Chatbot:" + str(bot.get_response(user_response)))
